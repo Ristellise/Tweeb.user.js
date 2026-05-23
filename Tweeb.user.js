d23397438543b60e7cc13b70b5a68662dd6f206b
Shinon <contakt@shinon71.moe>
2026-05-24 05:25:31 +0800

    date

--- Tweeb.user.js
+++ Tweeb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tweeb
 // @namespace    http://tampermonkey.net/
-// @version      10.05.13
+// @version      24.05.13
 // @description  Tweeb: Userscript for twitter
 // @author       Shinon
 // @match        https://twitter.com/*
